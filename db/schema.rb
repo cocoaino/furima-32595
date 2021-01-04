5f4adb3d1e6b328ec594c3ba131f9520fdb1e433
cocoaino <orionwonazoru2110@gmail.com>
2021-01-04 12:46:14 +0900

    itemモデルとitemテーブルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_25_215235) do
+ActiveRecord::Schema.define(version: 2021_01_04_032339) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "name", null: false
+    t.integer "price", null: false
+    t.text "description", null: false
+    t.integer "category_id", null: false
+    t.integer "status_id", null: false
+    t.integer "shipping_charges_id", null: false
+    t.integer "shipping_area_id", null: false
+    t.integer "shipping_days_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
