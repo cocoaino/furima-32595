70e3a8dd79efed6f6f64376c39da07b6cead345d
cocoaino <orionwonazoru2110@gmail.com>
2021-01-06 19:36:52 +0900

    バリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,12 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_one :order
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category, :status, :shipping_charge, :shipping_area, :shipping_day
+
+  validates :name, :price, :description, :image, presence: true
+  validates_inclusion_of :price, in: 300..9999999
+  validates :category_id, :status_id, :shipping_charge_id, :shipping_area_id, :shipping_day_id, numericality: { other_than: 1 }
 end
