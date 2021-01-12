956d9dbe6427c13f71a40a286c1efa4d5fd475e5
cocoaino <orionwonazoru2110@gmail.com>
2021-01-12 18:52:58 +0900

    決算機能実装

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,10 +1,11 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postal_code, :shipping_area_id, :city, :address, :building_name, :phone_number
+  attr_accessor :user_id, :item_id, :token, :postal_code, :shipping_area_id, :city, :address, :building_name, :phone_number
   
   with_options presence: true do
     validates :user_id
     validates :item_id
+    validates :token
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-).' }
     validates :shipping_area_id, numericality: { other_than: 1 }
     validates :city
