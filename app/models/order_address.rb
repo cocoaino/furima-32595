e702d92e37cc1aec170c47c76ffd03dc967f74f5
cocoaino <orionwonazoru2110@gmail.com>
2021-01-12 12:48:33 +0900

    商品購入ページのビューを追加

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,8 +1,20 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postal_code, :shipping_area_id, :city, :address, :buillding_name, :phone_number
+  attr_accessor :user_id, :item_id, :postal_code, :shipping_area_id, :city, :address, :building_name, :phone_number
+  
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+    validates :postal_code
+    validates :shipping_area_id
+    validates :city
+    validates :address
+    validates :building_name
+    validates :phone_number
+  end
+  
   def save
-    Order.create(user_id: user_id, item_id: item_id)
-    Address.create(postal_code: postal_code, shipping_area_id: shipping_area_id, city: city, address: address, buillding_name: buillding_name, phone_number: phone_number)
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Address.create(postal_code: postal_code, shipping_area_id: shipping_area_id, city: city, address: address, building_name: building_name, phone_number: phone_number)
   end
 end
